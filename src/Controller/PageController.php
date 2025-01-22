1e51f521a705c5557c76a1ec5992ab606d6d9650
Jensone <hello@jensone.com>
2025-01-22 10:20:25 +0100

    PageController with 4 new routes and templates

---
+++ src/Controller/PageController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PageController extends AbstractController
+{
+    // Page d'accueil 
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function home(): Response
+    {
+        return $this->render('page/home.html.twig', [
+            'controller_name' => 'HOME',
+        ]);
+    }
+
+    // Page contact
+    #[Route('/contact', name: 'app_contact', methods: ['GET', 'POST'])]
+    public function contact(): Response
+    {
+        return $this->render('page/contact.html.twig', [
+            'controller_name' => 'COUNTACTE',
+        ]);
+    }
+
+    // CGU
+    #[Route('/cgu', name: 'app_cgu', methods: ['GET'])]
+    public function cgu(): Response
+    {
+        return $this->render('page/cgu.html.twig', [
+            'controller_name' => 'CJIYUUU',
+        ]);
+    }
+
+    // RGPD
+    #[Route('/rgpd', name: 'app_rgpd', methods: ['GET'])]
+    public function rgpd(): Response
+    {
+        return $this->render('page/rgpd.html.twig', [
+            'controller_name' => 'RGGGP',
+        ]);
+    }
+}
